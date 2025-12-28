21ba87eeb1a068534175d8ae9234780714494b50
Zahra Mehmandoust <138685568+zahra2002-20@users.noreply.github.com>
2025-12-28 21:08:11 +0430

    PersonalInfoCard
    Personal Information Card!

---
+++ src/components/personalInfoCard.jsx
@@ -0,0 +1,14 @@
+import Row from "./Row";
+export default function PersonalInfoCard({ info }) {
+  return (
+    <div className="card infoCard">
+      <h2 className="cardTitle">Personal Information</h2>
+
+      <div className="rows">
+        {Object.keys(info).map((label) => {
+          <Row label={label} value={info[label]}></Row>;
+        })}
+      </div>
+    </div>
+  );
+}
